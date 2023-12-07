27dd0695a70c18695b60b3b42080bf09984545ec
salman <70580281+salman9011@users.noreply.github.com>
2023-12-07 11:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ else if(typeof value1==typeof value2){
 }
 
 // Get user input using prompt
-// let value1 = prompt("Enter Start of the Range.");
-// let value2 = prompt("Enter End Of the Range.");
+let value1 = prompt("Enter Start of the Range.");
+let value2 = prompt("Enter End Of the Range.");
 
 // Display the result
  alert(isSameType(value1, value2));
